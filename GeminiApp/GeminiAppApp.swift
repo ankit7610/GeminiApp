8bbbccf8011e317ad37d7bc5cc40f350cfc665b4
ankit7610 <kaushikankit2021@gmail.com>
2025-09-13 13:38:45 +0530

    Initial Commit

---
+++ GeminiApp/GeminiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAppApp.swift
+//  GeminiApp
+//
+//  Created by Ankit Kaushik on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
